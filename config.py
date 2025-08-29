7c73ffc7961b9f828b848158c6aa42f2ea4206ef
Nobi-123 <nobita.infringement.report@gmail.com>
2025-08-29 21:21:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID","-1003054036422"))
-GLOG = int(getenv("GLOG","waifu_for_u"))
+GLOG = int(getenv("GLOG","-1003054036422"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID","6395780961"))
